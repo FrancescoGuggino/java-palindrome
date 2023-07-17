8363c4830ebf50b77da5afb1503160ffa19ed179
FrancescoGuggino <landongu@gmail.com>
2023-07-17 14:21:57 +0200

    add word

--- src/org/java/palindrome/JavaPalindrome.java
+++ src/org/java/palindrome/JavaPalindrome.java
@@ -1,7 +1,15 @@
 package org.java.palindrome;
 
-public class JavaPalindrome {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class JavaPalindrome {
+    public static void main(String[] args){
+        //scrivere un programma che chiede all’utente
+        // di inserire una parola e gli risponde se è una parola palindroma oppure no
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Inserisci una parola: ");
+        String word = scan.nextLine();
     }
 }
